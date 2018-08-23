6201f2c82da706b6092d47077862bbb63fbcacd1
Silverfin13 <shahobmmousavi@gmail.com>
2018-08-22 21:41:32 -0700

    Initial commit

---
+++ commentBot/autopost.py
@@ -0,0 +1,20 @@
+#praw is required to connect with reddit API
+import praw
+import config
+
+def login():
+    r = praw.Reddit(username = config.username,
+                password = config.password,
+                client_id = config.client_id,
+                client_secret = config.client_secret,
+                user_agent = "HQ: Silverfin113")
+    return r
+
+def botCore(r):
+    for comment in r.subreddit('testingground4bots').comments(limit=25):
+        if "cat" in comment.body:
+            print("comment found")
+            comment.reply("thats a cat alright")
+
+r = login()
+botCore(r)
---
+++ commentBot/config.py
@@ -0,0 +1,6 @@
+username="yourUsername"
+password="yourPassword"
+
+# client id and secret are retrieved from the script configuration in https://www.reddit.com/prefs/
+client_id="id"
+client_secret="secret"
